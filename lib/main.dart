c074f5b8976df97987e3c846fd7c249ab98cebfc
syaifuddin zuhri <mochammadsyaifuddinz@gmail.com>
2021-12-09 13:36:25 +0700

    update title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CBT MANUSGI',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
